80f4efa729a281f7b35c75cdcd230079eee77705
Afsana Meem <afsumeem@gmail.com>
2023-10-04 23:07:43 +0600

    hero section design

--- src/components/UI/HeroSection.js
+++ src/components/UI/HeroSection.js
@@ -3,30 +3,27 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image
-          height={500}
-          width={500}
-          src=""
-          className="max-w-sm rounded-lg"
-          alt="banner"
-        />
-        <div className="md:text-center lg:text-left">
-          <h1 className="text-5xl font-bold">
-            Feast Your Eyes on <br /> A Good Book!
-          </h1>
-          <p className="py-6">
-            Books play a quintessential role in every student life by
-            introducing them to a world of imagination, providing knowledge of
-            the outside world, improving their reading, writing and speaking
-            skills as well as boosting memory and intelligence.
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://i.ibb.co/PMqXyjJ/intro-1654286079.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-70 bg-black"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-6xl uppercase font-bold">Tech Valley</h1>
+          <p className="mb-5">
+            Introducing Tech Valley, Your Ultimate Destination for Custom PC
+            Building!
+            <br />
+            Are you ready to elevate your gaming, design, or work experience to
+            unprecedented heights? Look no further than Tech Valley – the
+            premier online destination for enthusiasts and professionals alike
+            who crave power, performance, and precision in their computing
+            devices.
           </p>
-          <Link href="books">
-            <button className="btn text-black hover:bg-black bg-slate-400 hover:text-white transition duration-1000 font-bold">
-              Build Your Own PC!
-            </button>
-          </Link>
+          <button className="btn btn-primary">Build your own PC</button>
         </div>
       </div>
     </div>
